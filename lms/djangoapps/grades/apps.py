14abd6c78fdec1490f5b122deaed4c059339db1b
Nimisha Asthagiri <nasthagiri@edx.org>
2018-01-08 21:14:08 -0500

    Update grades app to be a Django App Plugin

--- lms/djangoapps/grades/apps.py
+++ lms/djangoapps/grades/apps.py
@@ -7,6 +7,7 @@ Signal handlers are connected here.
 from django.apps import AppConfig
 from django.conf import settings
 from edx_proctoring.runtime import set_runtime_service
+from openedx.core.djangolib.django_plugins import ProjectType, SettingsType, PluginURLs, PluginSettings
 
 
 class GradesConfig(AppConfig):
@@ -14,6 +15,23 @@ class GradesConfig(AppConfig):
     Application Configuration for Grades.
     """
     name = u'lms.djangoapps.grades'
+
+    plugin_app = {
+        PluginURLs.config: {
+            ProjectType.lms: {
+                PluginURLs.namespace: u'grades_api',
+                PluginURLs.regex: u'api/grades/',
+                PluginURLs.relative_path: u'api.urls',
+            }
+        },
+        PluginSettings.config: {
+            ProjectType.lms: {
+                SettingsType.aws: {PluginSettings.relative_path: u'settings.aws'},
+                SettingsType.common: {PluginSettings.relative_path: u'settings.common'},
+                SettingsType.test: {PluginSettings.relative_path: u'settings.test'},
+            }
+        }
+    }
 
     def ready(self):
         """
