fb66763c37df0b364ee9ca108de7c35c11a6aeed
Nimisha Asthagiri <nasthagiri@edx.org>
2018-01-08 21:14:42 -0500

    Update theming app to be a Django App Plugin

--- openedx/core/djangoapps/theming/apps.py
+++ openedx/core/djangoapps/theming/apps.py
@@ -1,9 +1,19 @@
 
 from django.apps import AppConfig
+from openedx.core.djangolib.django_plugins import ProjectType, PluginURLs
+
+
+plugin_urls_config = {PluginURLs.namespace: u'theming', PluginURLs.regex: u'theming/'}
 
 
 class ThemingConfig(AppConfig):
     name = 'openedx.core.djangoapps.theming'
+    plugin_app = {
+        PluginURLs.config: {
+            ProjectType.cms: plugin_urls_config,
+            ProjectType.lms: plugin_urls_config,
+        }
+    }
     verbose_name = "Theming"
 
     def ready(self):
